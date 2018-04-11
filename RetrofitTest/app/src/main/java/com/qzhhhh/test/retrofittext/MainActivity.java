ba995222ec375584f481bdb812d30d64ae3a9085
Qzhhh <qzhhh@QzhhhdeMacBook-Pro.local>
2018-04-11 09:49:44 +0800

    project

---
+++ RetrofitTest/app/src/main/java/com/qzhhhh/test/retrofittext/MainActivity.java
@@ -0,0 +1,13 @@
+package com.qzhhhh.test.retrofittext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
